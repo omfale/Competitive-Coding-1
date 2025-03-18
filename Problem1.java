726bcadeb364e90c206086c1b53e71450e9c81d2
om fale <omfale1001@gmail.com>
2025-03-18 12:49:16 -0400

    Done missing number from leetcode

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
 
+        int n = nums.length;
+
+        int expected_sum = n * (n+1)/2;
+
+        int actualSum = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            actualSum = actualSum + nums[i];
+        }
+
+        return expected_sum - actualSum;
+
+        
+    }
+}
